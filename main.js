d732894287490712b9c02a379ce965a4eb9f23fe
Nkechi Anyanwu <kechie,94@gmail.com>
2023-07-03 09:48:04 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+    constructor(fieldArray) {
+        this.field = fieldArray;
+    }
+    print() {
+       this.field.forEach(e => console.log(e.join('')));
+       
+       
+    }
+};
+const myField = new Field(
+    [['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░']]
+);
+
+myField.print();
